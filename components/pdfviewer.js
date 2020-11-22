91769c8f6f523f5c5f7edf0db02a8091255b2c9f
kmocorro <kevinmocorro.github@gmail.com>
2020-11-22 22:45:47 +0800

    okay

--- components/pdfviewer.js
+++ components/pdfviewer.js
@@ -21,30 +21,29 @@ const styles = StyleSheet.create({
   author: {
     fontSize: 12,
     textAlign: 'center',
-    marginBottom: 40,
+    marginBottom: 10,
   },
   subtitle: {
-    fontSize: 18,
-    margin: 12,
+    fontSize: 10,
+    margin: 6,
     fontFamily: 'Oswald'
   },
   text: {
-    margin: 12,
-    fontSize: 14,
+    margin: 8,
+    fontSize: 10,
     textAlign: 'justify',
     fontFamily: 'Times-Roman',
     wordWrap: 'break-word'
   },
   image: {
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    marginTop: 4,
-    marginBottom: 4,
-    width:'33%',
+    width:'50%',
+  },
+  imageRight: {
+    width:'50%',
   },
   header: {
     fontSize: 8,
-    marginBottom: 20,
+    marginBottom: 10,
     textAlign: 'left',
     color: 'grey',
   },
@@ -57,6 +56,34 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: 'grey',
   },
+  section: {
+    width: '100%',
+    display: 'table',
+  },
+  subsection: {
+    display: 'table-row',
+  },
+  cellsection: {
+    width: '50%',
+    display: 'table-cell',
+  },
+  container: {
+    display: 'flex',
+    '@media max-width: 400': {
+      flexDirection: 'column',
+    },
+    '@media min-width: 400': {
+      flexDirection: 'row',
+    },
+  },
+  fixed: {
+    width: '50%',
+    padding: 2
+  },
+  flexItem: {
+    flexGrow: 1,
+    padding: 4,
+  }
 });
 
 // Create Document Component
@@ -64,30 +91,38 @@ export default function MyDocument (props) {
   return (
   <PDFViewer height="720" width="100%">
   <Document>
-    <Page style={styles.body} orientation="landscape">
+    <Page style={styles.body}>
       <Text style={styles.header} fixed>
         Year: {props.bi.submission_year}{`    `}Week: {props.bi.submission_week} {`    `}ID: {props.bi.bi_id} {`    `}Status: {props.bi.current} {`    `}Benefactor: {props.bi.benefactor} {`    `}Initial Impact: {props.bi.initial_impact} {`    `}FA: {props.bi.fa} {`    `}FA Approver: {props.bi.fa_approver} {`    `}FA date: {props.bi.fa_date} {`    `}RA: {props.bi.ra} {`    `}RA Assessor: {props.bi.ra_assessor} {`    `}RA date: {props.bi.ra_date} {`    `}Action: {props.bi.action} {`    `}Action Owner: {props.bi.action_owner} {`    `}Action Date: {props.bi.action_date} {`    `}W/Denied Action: {props.bi.with_deniedAction} {`    `}Implementation: {props.bi.implementation} {`    `}Implementor: {props.bi.implementor} {`    `}Implementation Date: {props.bi.implementation_date}
       </Text>
       <Text style={styles.title}>{props.bi.title}</Text>
       <Text style={styles.author}>Employee No. : {props.bi.creator} {`    `} Shift: {props.bi.shift} {`    `} SPS Team: {props.bi.sps_team}</Text>
-      <Text style={{textAlign:'center', fontSize: '12', color: 'gray'}}>Before</Text>
-      <Image
-        style={styles.image}
-        src={`http://10.3.10.209:4546/images/${props.bi.before_imgPath}`}
-      />
-      {
-        props.bi.after_imgPath ? (
-          <>
-            <Text style={{textAlign:'center', fontSize: '12', color: 'gray'}}>After</Text>
+      <View style={styles.container}>
+        <View style={styles.fixed}>
+          <Text style={{fontSize: '12', color: 'gray'}}>Before</Text>
+          <Image
+            src={`http://10.3.10.209:4546/images/${props.bi.before_imgPath}`}
+          />
+        </View>
+        {
+          props.bi.after_imgPath ? (
+            <View style={styles.fixed}>
+              <Text style={{fontSize: '12', color: 'gray'}}>After</Text>
+              <Image
+                src={`http://10.3.10.209:4546/images/${props.bi.after_imgPath}`}
+              />
+            </View>
+          ):(
+          <View style={styles.fixed}>
+            <Text style={{fontSize: '12', color: 'gray'}}>After</Text>
             <Image
-              style={styles.image}
-              src={`http://10.3.10.209:4546/images/${props.bi.after_imgPath}`}
+              src={`/undraw_No_data_re_kwbl.png`}
+              style={{width: 150, height: 150}}
             />
-          </>
-        ):(
-          <></>
-        )
-      }
+          </View>
+          )
+        }
+      </View>
       <Text style={styles.subtitle}>
         Current Practice
       </Text>
